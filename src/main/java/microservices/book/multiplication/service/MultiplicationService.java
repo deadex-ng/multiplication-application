fd2a26ec08ac36746527975fb2b6b6bf08539e76
deadex-ng <thisisfumba@gmail.com>
2023-08-27 11:31:20 +0200

    java multiplication app

---
+++ src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -0,0 +1,6 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+public interface MultiplicationService {
+    Multiplication createRandomMultiplication();
+}
